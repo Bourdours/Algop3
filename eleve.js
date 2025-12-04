ef39e159d00c9212142c1dcf075269bc51183cde
Bourdours <maillot.gaetan@yahoo.fr>
2025-12-04 12:22:47 +0100

    realisation exercice eleve

---
+++ eleve.js
@@ -0,0 +1,12 @@
+        class Eleve {
+            constructor (n, p, c){
+                this.name = n ;
+                this.prenom = p ;
+                this.classe = c ;
+                this.notes = {
+                    maths : [],
+                    algorithmique : [],
+                    JavaScript : [] 
+                };
+            }
+        }
